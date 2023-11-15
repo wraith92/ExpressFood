3090b1224f1a053da5691b629a8c04ff2aaa3e56
hamza <benfaida.hamza2014@gmail.com>
2023-11-15 17:35:23 +0100

    décrémentation

--- server/routes/api/commandes.js
+++ server/routes/api/commandes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const commandes = require('../../models/Commandes');
+const plats = require('../../models/Plats');
 const jwtSecret = 'ma_cle_secrete'; 
 const { verifyToken } = require('./jwt');
 
@@ -55,5 +56,37 @@ router.put('/modifierStatut/:idCommande', verifyToken, async (req, res) => {
     res.status(500).json({ error: 'Erreur lors de la modification du statut de la commande' });
   }
 });
+
+
+router.put('/decrementerQuantite/:idCommande', verifyToken, async (req, res) => {
+  try {
+    const commandeId = req.params.idCommande;
+
+    const commande = await commandes.findById(commandeId);
+    if (!commande) {
+      return res.status(404).json({ error: 'Commande non trouvée' });
+    }
+
+    for (const plat of commande.plats) {
+      const platId = plat.plat;
+      const quantiteCommandee = plat.quantite;
+
+      const platExistant = await plats.findById(platId);
+      if (!platExistant) {
+        return res.status(404).json({ error: 'Plat non trouvé' });
+      }
+
+      platExistant.quantite -= quantiteCommandee;
+      await platExistant.save();
+    }
+
+    res.json({ success: true, message: 'Quantité des plats décrémentée avec succès' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erreur lors de la décrémentation de la quantité des plats' });
+  }
+});
+
+module.exports = router;
 
 module.exports = router;

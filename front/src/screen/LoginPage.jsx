ec4eecbb8e38caac2ff947c8f8ddc276a3ba3e0e
Abderrahman BENSALAH <bensalahabdelrrahman1@gmail.com>
2023-11-15 02:07:51 +0100

    navbar authorization

--- front/src/screen/LoginPage.jsx
+++ front/src/screen/LoginPage.jsx
@@ -1,7 +1,7 @@
-import React,{useEffect,useState} from "react";
-import { Link,useNavigate,useLocation } from "react-router-dom";
-import { Form,Button,Row,Col } from "react-bootstrap";
-import { useDispatch,useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import { loginUserAction } from "../action/AuthAction";
 import FormContainer from "../components/FormContainer";
 import Loading from "../components/Loading";
@@ -10,22 +10,29 @@ const LoginPage = () => {
     const navigate = useNavigate();
     const location = useLocation();
     const dispatch = useDispatch();
-    const [email,setEmail] = useState("");
-    const [motDePasse,setmotDePasse] = useState("");
-    const { loading,userInfo,error } = useSelector((state) => state.userLogin);
+    const [email, setEmail] = useState("");
+    const [motDePasse, setMotDePasse] = useState("");
+    const { loading, userinfo, error } = useSelector((state) => state.userLogin);
     const redirect = location.search ? location.search.split("=")[1] : "/";
 
-    useEffect(() => {
-        if (userInfo) {
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        await dispatch(loginUserAction({ email, motDePasse }));
+        const user = JSON.parse(localStorage.getItem("user"));
+        if (user) {
             navigate(redirect);
         }
-    },[navigate,redirect,userInfo]);
-
-    const submitHandler = (e) => {
-        e.preventDefault();
-        dispatch(loginUserAction({ email, motDePasse }));
+        // La redirection peut être effectuée directement dans le useEffect.
     };
-
+    
+    
+    useEffect(() => {
+        if (userinfo) {
+            localStorage.setItem("user", JSON.stringify(userinfo));
+            navigate(redirect);
+        }
+    }, [navigate, redirect, userinfo]);
+    
 
     return (
         <FormContainer>
@@ -35,17 +42,30 @@ const LoginPage = () => {
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId="email">
                     <Form.Label>Email Address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </Form.Group>
                 <Form.Group controlId="motDePasse">
-                    <Form.Label>motDePasse</Form.Label>
-                    <Form.Control type="password" placeholder="Enter motDePasse" value={motDePasse} onChange={(e) => setmotDePasse(e.target.value)}></Form.Control>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Enter password"
+                        value={motDePasse}
+                        onChange={(e) => setMotDePasse(e.target.value)}
+                    />
                 </Form.Group>
-                <Button type="submit" variant="primary">Sign In</Button>
+                <Button type="submit" variant="primary">
+                    Sign In
+                </Button>
             </Form>
             <Row className="py-3">
                 <Col>
-                    New Customer ? <Link to={redirect ? `/register?redirect=${redirect}` : "/register"}>Register</Link>
+                    New Customer?{" "}
+                    <Link to={redirect ? `/register?redirect=${redirect}` : "/register"}>Register</Link>
                 </Col>
             </Row>
         </FormContainer>

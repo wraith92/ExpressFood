678f018d77f183dd912d27466222f0d8482987ba
hamza <benfaida.hamza2014@gmail.com>
2023-11-17 12:43:14 +0100

    essai

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,17 +1,16 @@
-const mongoose=require('mongoose');
-const db= process.env.ATLAS_URL;
+const mongoose = require('mongoose');
+const db = process.env.ATLAS_URL;
 
-connectDB= async() =>{
-    console.log("connexion")
-    try{
-        mongoose.set('strictQuery',true);
-        await mongoose.connect(db);
-
+connectDB = async () => {
+    console.log("connexion");
+    try {
+        mongoose.set('strictQuery', true);
+        await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log("MongoDB à l'écoute");
-    }
-    catch(err){
+    } catch (err) {
         console.error(err.message);
         process.exit(1);
     }
 }
-module.exports=connectDB;
+
+module.exports = connectDB;

19a7ead1f43774822d0750f2c6078e808f39d79e
hamza <benfaida.hamza2014@gmail.com>
2023-11-17 14:31:54 +0100

    essai 3

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const db = process.env.ATLAS_URL;
 
+var c='mongodb+srv://group:group@projet.qzgbbxd.mongodb.net/Projet?retryWrites=true&w=majority'
+
 connectDB = async () => {
     console.log("connexion");
     try {
         mongoose.set('strictQuery', true);
-        await mongoose.connect('mongodb+srv://group:group@projet.qzgbbxd.mongodb.net/Projet?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(c, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log("MongoDB à l'écoute");
     } catch (err) {
         console.error(err.message);
